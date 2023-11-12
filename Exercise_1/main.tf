117f3ec46aec9b258f2991af74060e58a9b8839f
quoc.nguyen9595 <nguyenxuanquoc9595@gmail.com>
2023-11-12 23:27:14 +0700

    Update vpc

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,27 @@
 # TODO: Designate a cloud provider, region, and credentials
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 
+}
+
+provider "aws" {
+  profile = "adminacc"
+  region  = var.aws_region
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+# resource "aws_instance" "ec2T2" {
+#   ami           = "ami-05c13eab67c5d8861" # Amazon Linux
+#   instance_type = var.instance_type_t2
+# }
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+# resource "aws_instance" "ec2m4" {
+#   ami           = "ami-05c13eab67c5d8861" # Amazon Linux
+#   instance_type = var.instance_type_m4
+# }
---
+++ Exercise_1/variables.tf
@@ -0,0 +1,30 @@
+# AWS Region
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+  default = "us-east-1"  
+}
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "prod"
+}
+
+variable "instance_type_t2" {
+  description = "Value of the type for EC2 stance"
+  type = string
+  default = "t2.micro"
+}
+
+variable "instance_type_m4" {
+  description = "Value of the type for EC2 stance"
+  type = string
+  default = "m4.large"
+}
+
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "udacity"
+}
